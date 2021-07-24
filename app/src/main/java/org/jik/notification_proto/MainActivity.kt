1f965fb4f3d44f7cf4bed8a5c572ab7d03cdd0ce
jhg3410 <jhg34100@naver.com>
2021-07-24 19:42:12 +0900

    초기화면과 학과 선택화면(ver_1)

--- app/src/main/java/org/jik/notification_proto/MainActivity.kt
+++ app/src/main/java/org/jik/notification_proto/MainActivity.kt
@@ -3,10 +3,15 @@ package org.jik.notification_proto
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import androidx.appcompat.widget.AppCompatButton
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import org.jik.notification_proto.adapter.CustomAdpater
 import org.jik.notification_proto.data.Item
+import org.jik.notification_proto.fragment.Fragment_Initial
+import org.jik.notification_proto.fragment.Fragment_Selection
 import org.jsoup.Jsoup
 import org.jsoup.select.Elements
 import kotlin.concurrent.thread
@@ -15,52 +20,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        thread {
-            var result = ArrayList<Item>()
+        val initial =Fragment_Initial()
+        supportFragmentManager.beginTransaction().replace(R.id.main_content,initial).commit()
 
-            var array = arrayOf(1,2,3,4,5,6,7)
-            for(i in array) {
-                val doc =
-                        Jsoup.connect("https://www.inu.ac.kr/user/indexSub.do?codyMenuSeq=41719&siteId=ite&dum=dum&boardId=45113&page=$i")
-                                .get()
-                val get_class = doc.select(".list")
-                val titles = get_class.select(".title").select("a")
-                val date = get_class.select("tbody>tr>td")[8]
-                Log.d("date",date.toString())
-
-                for (title in titles) {
-                    val titletxt = title.text()
-                    val datetxt = date.text()
-                    Log.d("asd", titletxt)
-                    result.add(Item(titletxt, datetxt))
-                }
-            }
-
-
-
-            this.runOnUiThread {
-                val recyclerViewAdapter = CustomAdpater(result)
-                val linearLayoutManager = LinearLayoutManager(this)
-
-                val recyclerview = findViewById<RecyclerView>(R.id.recyclerView)
-
-                recyclerview.layoutManager = linearLayoutManager
-                recyclerview.adapter= recyclerViewAdapter
-
-
-            }
-        }
-    }
-
-
-    fun makeLineText(element: Elements) : String{
-        val list = element.html().split("<br>").toTypedArray()
-        var result = ""
-        list.forEach {
-            if(it == " ") return@forEach
-            result+=it+"\n"
-        }
-        return result
     }
 }
 
