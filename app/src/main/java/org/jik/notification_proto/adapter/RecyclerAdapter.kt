8ecf369fee325eec4d38d1f227b48128923b82b8
jhg3410 <jhg34100@naver.com>
2021-07-26 12:02:15 +0900

    학과추가, keyword layout(verson_1)

--- app/src/main/java/org/jik/notification_proto/adapter/RecyclerAdapter.kt
+++ app/src/main/java/org/jik/notification_proto/adapter/RecyclerAdapter.kt
@@ -1,5 +1,7 @@
 package org.jik.notification_proto.adapter
 
+import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,11 +9,24 @@ import androidx.appcompat.widget.AppCompatButton
 import androidx.recyclerview.widget.RecyclerView
 import org.jik.notification_proto.R
 import org.jik.notification_proto.data.College
+import org.jik.notification_proto.fragment.FragmentSelection
 
 class RecyclerAdapter(val colleges:MutableList<College>):RecyclerView.Adapter<RecyclerAdapter.Holder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.Holder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_college, parent,false)
+        var college_btn:AppCompatButton = view.findViewById(R.id.college_btn)
+        college_btn.setOnClickListener {
+            colleges.clear()
+            if (college_btn.text == "정보기술대학"){
+                colleges.add(College("컴퓨터공학부"))
+                colleges.add(College("정보통신공학과"))
+                colleges.add(College("임베디드시스템공학과"))
+            }
+            notifyDataSetChanged()
+        }
+
         return Holder(view)
+
     }
 
     override fun onBindViewHolder(holder: RecyclerAdapter.Holder, position: Int) {
@@ -24,6 +39,10 @@ class RecyclerAdapter(val colleges:MutableList<College>):RecyclerView.Adapter<Re
         private var college_btn:AppCompatButton = itemView.findViewById(R.id.college_btn)
         fun bind(college:College){
             college_btn.text = college.name
+
+
         }
+
     }
+
 }
