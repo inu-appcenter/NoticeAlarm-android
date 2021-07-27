1612f064f2f4a303582bef67c90a84ac77003372
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2021-07-27 11:16:31 +0900

    단대,학과 json으로 파싱

--- app/src/main/java/org/jik/notification_proto/fragment/FragmentSelection.kt
+++ app/src/main/java/org/jik/notification_proto/fragment/FragmentSelection.kt
@@ -1,21 +1,21 @@
 package org.jik.notification_proto.fragment
 
+import android.content.res.AssetManager
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.appcompat.widget.AppCompatButton
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import org.jik.notification_proto.R
 import org.jik.notification_proto.adapter.RecyclerAdapter
 import org.jik.notification_proto.data.College
+import org.json.JSONObject
 
 class FragmentSelection : Fragment() {
-    private val list = listOf("인문대학","자연과학대학","사회과학대학","글로벌정경대학","공과대학","정보기술대학","경영대학","예술체육대학")
     lateinit var recyclerview : RecyclerView
 
     override fun onCreateView(
@@ -25,9 +25,17 @@ class FragmentSelection : Fragment() {
         val view =  inflater.inflate(R.layout.fragment__selection, container, false)
         val collegelist :MutableList<College> = mutableListOf()
 
-        for(i in list){
-            collegelist.add(College(i))
-        }
+        // json 파일 가져와서 collegelist에 추가시켜주기
+        val assetManager: AssetManager = context?.resources?.assets!!
+        val inputStream= assetManager.open("json/INU.json")
+        val jsonString = inputStream.bufferedReader().use { it.readText() }
+        val jObject = JSONObject(jsonString)
+        val jsonArray = jObject.getJSONArray("INU")
+        for (i in 0 until jsonArray.length()) {
+            val iObject = jsonArray.getJSONObject(i)
+            val college = iObject.getString("college")
+            collegelist.add(College(college))
+       }
 
         recyclerview = view.findViewById(R.id.selection_recyclerView) as RecyclerView
         recyclerview.layoutManager = LinearLayoutManager(activity)
