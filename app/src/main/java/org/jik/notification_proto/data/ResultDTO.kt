9b349b8c3e8494edd9eb431f906f2f892c66ecb1
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2021-07-27 14:40:34 +0900

    firestore에 데이터쓰기를 구현했지만 userid가 null!! 로그인 구현? 다른방법?

---
+++ app/src/main/java/org/jik/notification_proto/data/ResultDTO.kt
@@ -0,0 +1,6 @@
+package org.jik.notification_proto.data
+
+data class ResultDTO(
+    var uid:String? = null,
+    var keyword : String?= null
+)
--- app/src/main/java/org/jik/notification_proto/fragment/FragmentKeyword.kt
+++ app/src/main/java/org/jik/notification_proto/fragment/FragmentKeyword.kt
@@ -5,25 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.widget.AppCompatButton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import org.jik.notification_proto.R
+import org.jik.notification_proto.data.ResultDTO
 
 
 class FragmentKeyword : Fragment() {
+    var auth : FirebaseAuth? =null
+    var firestore : FirebaseFirestore? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view =  inflater.inflate(R.layout.fragment_keyword, container, false)
 
+        auth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
+
+
         view.findViewById<TextView>(R.id.delete).setOnClickListener {
             view.findViewById<EditText>(R.id.edit_keyword).text.clear()
         }
 
         view.findViewById<AppCompatButton>(R.id.enroll_btn).setOnClickListener {
+            var resultDTO = ResultDTO()
+            resultDTO.uid = auth?.currentUser?.uid
+            resultDTO.keyword = view.findViewById<EditText>(R.id.edit_keyword).text.toString()
+            firestore?.collection("users")?.document(auth?.uid.toString())?.set(resultDTO)
+
             if (view.findViewById<TextView>(R.id.textView1)?.visibility == View.INVISIBLE){
                 view.findViewById<TextView>(R.id.textView1).text = "#" + view.findViewById<EditText>(R.id.edit_keyword).text
                 view.findViewById<TextView>(R.id.textView1).visibility = View.VISIBLE
