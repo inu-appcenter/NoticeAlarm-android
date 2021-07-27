11d2ab1c47479c6f310fd3e86e0d7fca59d0f538
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2021-07-27 13:37:00 +0900

    keyword verson_2

--- app/src/main/java/org/jik/notification_proto/fragment/FragmentKeyword.kt
+++ app/src/main/java/org/jik/notification_proto/fragment/FragmentKeyword.kt
@@ -19,9 +19,28 @@ class FragmentKeyword : Fragment() {
     ): View? {
         val view =  inflater.inflate(R.layout.fragment_keyword, container, false)
 
+        view.findViewById<TextView>(R.id.delete).setOnClickListener {
+            view.findViewById<EditText>(R.id.edit_keyword).text.clear()
+        }
+
         view.findViewById<AppCompatButton>(R.id.enroll_btn).setOnClickListener {
-            view.findViewById<TextView>(R.id.textView10).text = "#" + view.findViewById<EditText>(R.id.edit_keyword).text
-            view.findViewById<TextView>(R.id.textView10).visibility= View.VISIBLE
+            if (view.findViewById<TextView>(R.id.textView1)?.visibility == View.INVISIBLE){
+                view.findViewById<TextView>(R.id.textView1).text = "#" + view.findViewById<EditText>(R.id.edit_keyword).text
+                view.findViewById<TextView>(R.id.textView1).visibility = View.VISIBLE
+            }
+            else if (view.findViewById<TextView>(R.id.textView1)?.visibility == View.VISIBLE && view.findViewById<TextView>(R.id.textView2)?.visibility == View.INVISIBLE){
+                view.findViewById<TextView>(R.id.textView2).text = "#" + view.findViewById<EditText>(R.id.edit_keyword).text
+                view.findViewById<TextView>(R.id.textView2).visibility = View.VISIBLE
+
+            }
+            else if (view.findViewById<TextView>(R.id.textView2)?.visibility == View.VISIBLE && view.findViewById<TextView>(R.id.textView3)?.visibility == View.INVISIBLE){
+                view.findViewById<TextView>(R.id.textView3).text = "#" + view.findViewById<EditText>(R.id.edit_keyword).text
+                view.findViewById<TextView>(R.id.textView3).visibility = View.VISIBLE
+            }
+            else if (view.findViewById<TextView>(R.id.textView3)?.visibility == View.VISIBLE){
+                view.findViewById<TextView>(R.id.textView4).text = "#" + view.findViewById<EditText>(R.id.edit_keyword).text
+                view.findViewById<TextView>(R.id.textView4).visibility = View.VISIBLE
+            }
         }
 
         return view
