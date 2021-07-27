b6f932c49cd799b41cea39373c8d7d85dd57ff30
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2021-07-27 11:16:31 +0900

    단대,학과 json으로 파싱

--- app/src/main/java/org/jik/notification_proto/adapter/RecyclerAdapter.kt
+++ app/src/main/java/org/jik/notification_proto/adapter/RecyclerAdapter.kt
@@ -1,5 +1,6 @@
 package org.jik.notification_proto.adapter
 
+import android.content.res.AssetManager
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -10,27 +11,38 @@ import androidx.recyclerview.widget.RecyclerView
 import org.jik.notification_proto.R
 import org.jik.notification_proto.data.College
 import org.jik.notification_proto.fragment.FragmentSelection
+import org.json.JSONObject
+import kotlin.collections.listOf as listOf1
 
 class RecyclerAdapter(val colleges:MutableList<College>):RecyclerView.Adapter<RecyclerAdapter.Holder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.Holder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_college, parent,false)
-        var college_btn:AppCompatButton = view.findViewById(R.id.college_btn)
-        college_btn.setOnClickListener {
-            colleges.clear()
-            if (college_btn.text == "정보기술대학"){
-                colleges.add(College("컴퓨터공학부"))
-                colleges.add(College("정보통신공학과"))
-                colleges.add(College("임베디드시스템공학과"))
-            }
-            notifyDataSetChanged()
-        }
-
         return Holder(view)
 
     }
 
     override fun onBindViewHolder(holder: RecyclerAdapter.Holder, position: Int) {
         holder.bind(colleges[position])
+
+        // 단대 버튼이 클릭되면 json파일을 가져와 위치가 동일한 (같은) 단대의 학과들을 colleges에 추가시킴으로 view 업데이트
+        holder.itemView.findViewById<AppCompatButton>(R.id.college_btn).setOnClickListener {
+            colleges.clear()
+            val assetManager: AssetManager = holder.itemView.resources?.assets!!
+            val inputStream = assetManager.open("json/INU.json")
+            val jsonString = inputStream.bufferedReader().use { it.readText() }
+            val jObject = JSONObject(jsonString)
+            val jsonArray = jObject.getJSONArray("INU")
+            for (i in 0 until jsonArray.length()) {
+                if (i == holder.adapterPosition) {
+                    val iObject = jsonArray.getJSONObject(i)
+                    val major = iObject.getJSONArray("major")
+                    for (j in 0 until major.length()){
+                         colleges.add(College(major[j] as String))
+                    }
+                }
+            }
+            notifyDataSetChanged()
+        }
     }
 
     override fun getItemCount(): Int = colleges.size
@@ -39,10 +51,6 @@ class RecyclerAdapter(val colleges:MutableList<College>):RecyclerView.Adapter<Re
         private var college_btn:AppCompatButton = itemView.findViewById(R.id.college_btn)
         fun bind(college:College){
             college_btn.text = college.name
-
-
         }
-
     }
-
 }
